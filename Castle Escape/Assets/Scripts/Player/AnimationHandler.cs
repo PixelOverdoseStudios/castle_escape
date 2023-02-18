7f96c9d8ef7871056f5b2122a7523741a0d1627f
PixelOverdoseStudios <123806965+PixelOverdoseStudios@users.noreply.github.com>
2023-02-18 14:21:47 -0600

    Player Created
    Player is created, with a cinemachine camera, and basic tilemats.

---
+++ Castle Escape/Assets/Scripts/Player/AnimationHandler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHandler : MonoBehaviour
+{
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    public void PlayRun(int run)
+    {
+        animator.SetInteger("run", run);
+    }
+
+    public void PlayJumpAndFall (int jump)
+    {
+        animator.SetInteger("jump", jump);
+    }
+
+    public void SwingSword()
+    {
+        animator.SetTrigger("attack");
+    }
+}
---
+++ Castle Escape/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed, jumpForce, attackTimer;
+    [SerializeField] LayerMask groundMask;
+
+    private Rigidbody2D rig;
+    private SpriteRenderer sr;
+    private AnimationHandler animHandler;
+    private RaycastHit2D groundCast;
+    private CapsuleCollider2D capCol2D;
+    private float nextFireTime = 0;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        capCol2D = GetComponent<CapsuleCollider2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        animHandler = GetComponent<AnimationHandler>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rig.velocity = new Vector2 (moveInput * moveSpeed, rig.velocity.y);
+
+        if (rig.velocity.x < 0f)
+        {
+            sr.flipX = true;
+        }
+        else if (rig.velocity.x > 0f)
+        {
+            sr.flipX = false;
+        }
+    }
+
+    void Update()
+    {
+        HandleAnimation();
+        HandleJumping();
+        Attack();
+    }
+
+    void HandleAnimation()
+    {
+        animHandler.PlayRun(Mathf.Abs((int)rig.velocity.x));
+        animHandler.PlayJumpAndFall((int)rig.velocity.y);
+    }
+    
+    void HandleJumping()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (IsGrounded())
+            {
+                Jump();
+            }
+        }
+    }
+
+    bool IsGrounded()
+    {
+        groundCast = Physics2D.BoxCast(capCol2D.bounds.center, capCol2D.bounds.size, 0f, Vector2.down, 0.25f, groundMask);
+        return groundCast.collider != null;
+    }
+
+    void Jump()
+    {
+        rig.velocity = Vector2.up * jumpForce;
+    }
+
+    void Attack()
+    {
+        if (Time.time > nextFireTime)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                animHandler.SwingSword();
+                nextFireTime = Time.time + attackTimer;
+            }
+        }
+    }
+}
